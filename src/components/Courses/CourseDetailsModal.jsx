5e206307b5ca03661fe75caadfe16cf2cdab82a7
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2024-11-21 23:28:55 +0600

    Update CourseDetailsModal.jsx
    Fixed CourseDetails Modal width

--- src/components/Courses/CourseDetailsModal.jsx
+++ src/components/Courses/CourseDetailsModal.jsx
@@ -6,7 +6,7 @@ export default function CourseDetailsModal({ course, closeModal, openEnrollModal
    const handleClose = () => {
       // Trigger the closing animation
       setIsClosing(true);
-      
+
       // Wait for the animation to finish before calling the closeModal function
       setTimeout(() => {
          closeModal(); // Close the modal after the animation
@@ -18,7 +18,7 @@ export default function CourseDetailsModal({ course, closeModal, openEnrollModal
    return (
       <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
          <div
-            className={`bg-white rounded-lg shadow-lg w-96 p-6 ${isClosing ? 'animate-slide-down' : 'animate-slide-up'}`}
+            className={`bg-white rounded-lg shadow-lg max-w-[600px] w-11/12 p-6 ${isClosing ? 'animate-slide-down' : 'animate-slide-up'}`}
          >
             <div className="flex justify-between items-center">
                <h2 className="text-2xl font-semibold text-emerald-900">{course.title}</h2>
